6185d0fb6957e6392e0e0994082071db640f9746
Saksham Agrahari <agrahari0899@gmail.com>
2026-03-09 12:31:19 +0530

    Add file

---
+++ Java_Collections_Framework/src/collections/Demo.java
@@ -0,0 +1,53 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.TreeSet;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+
+public class Demo {
+
+    public static void main(String[] args) {
+
+//        Object obj[] = {1, 'A', "B"};
+//
+//        for (int i = 0; i < obj.length; i++) {
+//            System.out.println(obj[i]);
+//        }
+//
+//        TreeSet set = new TreeSet();
+//        set.add("A");
+//        set.add(null);                     // Null pointer exception
+//        set.add(1);
+//
+//        System.out.println(set);
+
+
+//        ArrayList<Integer> list = new ArrayList<>();
+//
+//        list.add(1);
+//        list.add(2);
+//        list.add(3);
+//
+//        for(Integer i : list){             // failfast concept in this exception using the modcount check variable.
+//            if(i.equals(1)){
+//                list.remove(i);
+//            }
+//            System.out.println(i);         // ConcurrentModificationException (because of the 1 remove)
+//        }
+
+
+//        List<Integer> al = new CopyOnWriteArrayList<>();
+//
+//        al.add(1);
+//        al.add(2);
+//        al.add(3);
+//
+//        for(Integer i : al){
+//            if(i.equals(1)){
+//                al.remove(i);
+//            }
+//            System.out.println(i);
+//        }
+    }
+}
